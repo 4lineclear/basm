27d5969f91388a72b30a655e9d183209be90a70a
4lineclear <tamimhakimi21@gmail.com>
2025-01-16 16:40:46 -0500

    adding ls stuff, refactored

--- basm-parse/src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod lex;
-
-// type LineResult = Result<Line, >
-//
-// /// Parses each line
-// pub fn lex_lines(input: &str) -> impl Iterator<Item = IResult<Span, Line>> {
-//     lex::lex_lines(input).map(|r|)
-// }
